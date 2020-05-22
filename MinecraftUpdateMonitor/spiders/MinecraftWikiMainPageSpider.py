05555eb033f7707e0372d805065a8e367a302139
Jim Shi <shiyilin2008@gmail.com>
2020-05-22 12:50:21 -0700

    Updates XPaths in the main page spider.

--- MinecraftUpdateMonitor/spiders/MinecraftWikiMainPageSpider.py
+++ MinecraftUpdateMonitor/spiders/MinecraftWikiMainPageSpider.py
@@ -19,22 +19,27 @@ class MinecraftWikiMainPageSpider (scrapy.Spider):
     def parse(self, response):
         try:
             javaEditionVersionStrings = response.xpath("//div[@class='edition-group']/div[@class='edition-box'][1]/div/div/a[@title='Java']/../b/a[@href and @title]/text()").extract()
-            javaEditionRelativeURLs = response.xpath("//div[@class='edition-group']/div[@class='edition-box'][1]/div/div/a[@title='Java']/../b/a[@href and @title]/@href").extract()
 
-            if ((len(javaEditionVersionStrings) == 1) and (len(javaEditionRelativeURLs) == 1)):
+            releaseVersionString = response.xpath("//div[@class='edition-group']/div[@class='edition-box'][1]/div[1]/div[1]/b/a[@href and @title]/text()").extract()[0]
+            releaseVersionRelativeURL = response.xpath("//div[@class='edition-group']/div[@class='edition-box'][1]/div[1]/div[1]/b/a[@href and @title]/@href").extract()[0]
+
+            if (len(javaEditionVersionStrings) == 2):
                 # Only a release version is available.
                 yield {
-                    "releaseVersionString": javaEditionVersionStrings[0],
-                    "releaseVersionURL": "https://minecraft.gamepedia.com" + javaEditionRelativeURLs[0]
+                    "releaseVersionString": releaseVersionString,
+                    "releaseVersionURL": "https://minecraft.gamepedia.com" + releaseVersionRelativeURL
                 }
 
-            elif ((len(javaEditionVersionStrings) == 2) and (len(javaEditionRelativeURLs) == 2)):
+            elif (len(javaEditionVersionStrings) == 3):
                 # Both a release version and a development version are available.
+                developmentVersionString = response.xpath("//div[@class='edition-group']/div[@class='edition-box'][1]/div[1]/div[2]/a[@href and @title][2]/text()").extract()[0]
+                developmentVersionRelativeURL = response.xpath("//div[@class='edition-group']/div[@class='edition-box'][1]/div[1]/div[2]/a[@href and @title][2]/@href").extract()[0]
+
                 yield {
-                    "releaseVersionString": javaEditionVersionStrings[0],
-                    "releaseVersionURL": "https://minecraft.gamepedia.com" + javaEditionRelativeURLs[0],
-                    "developmentVersionString": javaEditionVersionStrings[1],
-                    "developmentVersionURL": "https://minecraft.gamepedia.com" + javaEditionRelativeURLs[1]
+                    "releaseVersionString": releaseVersionString,
+                    "releaseVersionURL": "https://minecraft.gamepedia.com" + releaseVersionRelativeURL,
+                    "developmentVersionString": developmentVersionString,
+                    "developmentVersionURL": "https://minecraft.gamepedia.com" + developmentVersionRelativeURL
                 }
 
             else:
